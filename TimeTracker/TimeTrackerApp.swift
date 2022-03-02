bc62eff21f475a76ca5e2f7791effd5221397dc1
Vyacheslav Beltyukov <viacheslav@beltyukov.mobi>
2022-03-02 17:57:00 +0200

    Initial Commit

---
+++ TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Вячеслав Бельтюков on 02.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
